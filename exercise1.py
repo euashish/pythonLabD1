d70081f57dab3dc9f2fca6201250418db6ae572b
ASHISH <ashish.adhikary.2003@gmail.com>
2026-07-06 08:21:34 +0600

    work on some exercise

---
+++ exercise1.py
@@ -0,0 +1,8 @@
+# Exercise 1 Rectangle Area Calculation : Try Yourself
+
+length = float(input("Enter the length :"))
+width = float(input("Enter the width :"))
+area = length * width
+print(f"The area is : {area}cm²")
+
+# cm square can not written in vs code or python as cm^2. It can be written as : Windows : NumLock + Alt + 0178
---
+++ task5.py
@@ -0,0 +1,20 @@
+# input() is a function that prompts the user to enter data and returns it as a string.
+
+# To view wrapped text and comments in one page: press Alt + Z.
+
+# name = input("What is your name? :")
+# age = input("How old are you?")
+
+# age = int(age)
+# age = age + 1
+
+# That can be written in simplified form in bellow...
+
+
+name = input("What is your name? :")
+age = int(input("How old are you?"))
+age = age + 1
+
+print(f"Hello {name}!")
+print("HAPPY BIRTHDAY!!!!")
+print(f"You are {age} years old")
